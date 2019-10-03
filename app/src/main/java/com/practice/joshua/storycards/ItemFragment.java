e34da747728b5502363ee7dc5a5fa48cbcd481f0
Joshua Cadavez <cadav001@umn.edu>
2019-10-02 21:21:46 -0400

    [0001] Added ViewPager with back press button to previous image. Next plan is to add images via camera assets

---
+++ app/src/main/java/com/practice/joshua/storycards/ItemFragment.java
@@ -0,0 +1,66 @@
+package com.practice.joshua.storycards;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+
+public class ItemFragment extends Fragment {
+
+    private Story mStory;
+    private static final String STORY_KEY = "story_key";
+
+
+    public ItemFragment() {
+    }
+
+    public static ItemFragment newInstance(Story story) {
+
+        Bundle args = new Bundle();
+        args.putParcelable(STORY_KEY, story);
+
+        ItemFragment fragment = new ItemFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup) inflater
+                .inflate(R.layout.fragment_item, container, false);
+
+        Bundle args = getArguments();
+        if (args == null) throw new AssertionError();
+
+        mStory = args.getParcelable(STORY_KEY);
+        if (mStory == null) throw new AssertionError();
+
+//      display text and image
+        TextView nameText = (TextView) rootView.findViewById(R.id.nameText);
+        nameText.setText(mStory.getName());
+
+        TextView descriptionText = (TextView) rootView.findViewById(R.id.descriptionText);
+        descriptionText.setText(mStory.getDescription());
+
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        String price = formatter.format(mStory.getPage());
+        TextView priceText = (TextView) rootView.findViewById(R.id.pageText);
+        priceText.setText(price);
+
+        String productId = mStory.getStoryId();
+        int imageResource = getActivity().getResources()
+                .getIdentifier(productId, "drawable", getActivity().getPackageName());
+        ImageView iv = (ImageView) rootView.findViewById(R.id.imageView);
+        iv.setImageResource(imageResource);
+
+        return rootView;
+    }
+}
--- app/src/main/java/com/practice/joshua/storycards/MainActivity.java
+++ app/src/main/java/com/practice/joshua/storycards/MainActivity.java
@@ -1,13 +1,60 @@
 package com.practice.joshua.storycards;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private final List<Story> stories = DataProvider.storyList;
+    private final int numPages = stories.size();
+
+    private ViewPager mPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPager = findViewById(R.id.main_pager);
+        PagerAdapter pagerAdapter =
+                new ViewPagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(pagerAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) super.onBackPressed();
+        else mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+    }
+
+    private class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        /*
+            Get a fragment and display it
+         */
+        @Override
+        public Fragment getItem(int pos) {
+            return ItemFragment.newInstance(stories.get(pos));
+        }
+
+        /*
+            Determine how many pages to create
+         */
+        @Override
+        public int getCount() {
+            return numPages;
+        }
     }
 }
